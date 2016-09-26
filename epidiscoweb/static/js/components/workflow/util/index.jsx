dc82722a3ca1381cd0c9fd85b87db931abcd083e
B. Arman Aksoy <arman@aksoy.org>
2016-09-26 17:42:24 -0400

    add tools to the empty workflow object

--- epidiscoweb/static/js/components/workflow/util/index.jsx
+++ epidiscoweb/static/js/components/workflow/util/index.jsx
@@ -7,7 +7,12 @@ const createEmptyWorkflow = (workflowId) => ({
   normal: { files: [] },
   tumor: { files: [] },
   rna: { files: [] },
-  tools: []
+  tools: [
+    { name: "MuTect-2", disabled: false, run: false },
+    { name: "seq2HLA", disabled: false, run: true },
+    { name: "SomaticSniper", disabled: false, run: true },
+    { name: "VarScan", disabled: false, run: false }
+  ]
 });
 
 const fetchWorkflow = (workflowId, callback) => {
