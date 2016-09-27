a7c36f8fb950bb1651f44b46418a2c9452037891
B. Arman Aksoy <arman@aksoy.org>
2016-09-27 01:40:34 -0400

    hla is not part of the desc

--- epidiscoweb/static/js/components/workflow/create/steps/description.jsx
+++ epidiscoweb/static/js/components/workflow/create/steps/description.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import Chip from 'material-ui/Chip';
-import TextField from 'material-ui/TextField';
-import LocalOffer from 'material-ui/svg-icons/maps/local-offer';
-
 import {Grid, Row, Col} from 'react-flexbox-grid/lib';
 import {Step, StepButton, StepContent} from 'material-ui/Stepper';
+import TextField from 'material-ui/TextField';
 
 import EpiSection from '../section';
 
@@ -29,13 +26,6 @@ const EpiDescriptionStep = (props) => (
 
 const EpiDescription = (props) => {
   const desc = props.workflow.description;
-  const chips = desc.tags.map(
-    (t, i) => (
-      <Chip key={i} className={style.chip} style={{margin: 5}}>
-        {t}
-      </Chip>
-    )
-  );
 
   return (
     <EpiSection title="Workflow details">
@@ -44,7 +34,7 @@ const EpiDescription = (props) => {
           <Col xs={12}>
             <TextField
               hintText="Lung Cancer Patient #42"
-              floatingLabelText="Run Name"
+              floatingLabelText="Descriptive Name"
               value={desc.name}
               fullWidth={true}
             />
@@ -57,29 +47,6 @@ const EpiDescription = (props) => {
               floatingLabelText="E-mail address"
               value={desc.email}
               fullWidth={true}
-            />
-          </Col>
-        </Row>
-        <Row middle="xs" center="xs">
-          <Col xs={1}>
-            <LocalOffer />
-          </Col>
-          <Col xs={11}>
-            <div className={style.chips}>
-              {chips}
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12}>
-            <TextField
-              hintText={"HLA-A*01:01\nHLA-A*02:01"}
-              floatingLabelText="HLA Types (one per line - optional)"
-              multiLine={true}
-              fullWidth={true}
-              rows={2}
-              rowsMax={6}
-              value={desc.hlas.join("\n")}
             />
           </Col>
         </Row>
