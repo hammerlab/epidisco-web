2814f350d65b91fef9f63439c6d83bea3fa93d10
Isaac Hodes <isaachodes@gmail.com>
2016-10-04 16:28:44 -0400

    Listen to Flask lint

--- epidiscoweb/email.py
+++ epidiscoweb/email.py
@@ -1,47 +1,49 @@
-# Sending emails
-from epidiscoweb import app
-
+"""Module for sending notification emails to users and admins."""
 import requests
 
+from epidiscoweb import app
+
 REQUIRED_MAILGUN_KEYS = \
-  ["MAILGUN_API_KEY", "MAILGUN_VALIDATION_KEY", "MAILGUN_DOMAIN", "ADMIN_FROM_EMAIL"]
+  ['MAILGUN_API_KEY', 'MAILGUN_VALIDATION_KEY', 'MAILGUN_DOMAIN', 'ADMIN_FROM_EMAIL']
 
 
-def can_send_emails():
-    disabled = app.config.get("DISABLE_EMAILS")
-    if disabled and disabled.upper() == "TRUE":
+def _can_send_emails():
+    disabled = app.config.get('DISABLE_EMAILS')
+    if disabled and disabled.upper() == 'TRUE':
         return False
     else:
         for key in REQUIRED_MAILGUN_KEYS:
             if key not in app.config:
-                raise KeyError("Missing required key in config: {}".format(key))
+                raise KeyError('Missing required key in config: {}'.format(key))
         return True
 
 
 def send_receipt_email(user, workflow):
-    if not can_send_emails():
+    """Send user submitting workflow an email confirming receipt of the workflow."""
+    if not _can_send_emails():
         return False
-    message = "You'll be hearing from us soon with news\
-    about your workflow, id {}".format(workflow.workflow_id)
+    message = 'You\'ll be hearing from us soon with news\
+    about your workflow, id {}'.format(workflow.workflow_id)
     return requests.post(
-        "https://api.mailgun.net/v3/{}/messages".format(app.config["MAILGUN_DOMAIN"]),
-        auth=("api", app.config["MAILGUN_API_KEY"]),
-        data={"from": app.config["ADMIN_FROM_EMAIL"],
-              "to": [user.email],
-              "subject": "Your Epidisco Workflow was submitted",
-              "text": message})
+        'https://api.mailgun.net/v3/{}/messages'.format(app.config['MAILGUN_DOMAIN']),
+        auth=('api', app.config['MAILGUN_API_KEY']),
+        data={'from': app.config['ADMIN_FROM_EMAIL'],
+              'to': [user.email],
+              'subject': 'Your Epidisco Workflow was submitted',
+              'text': message})
 
 
 def send_admin_notify_email(user, workflow):
-    if not can_send_emails():
+    """Send the admin an email notifying them about a newly submitted workflow."""
+    if not _can_send_emails():
         return False
-    message = "Workflow {} was just submitted by {}"\
+    message = 'Workflow {} was just submitted by {}'\
               .format(workflow.workflow_id, user.email)
-    admin = app.config["ADMIN_FROM_EMAIL"]
+    admin = app.config['ADMIN_FROM_EMAIL']
     return requests.post(
-        "https://api.mailgun.net/v3/{}/messages".format(app.config["MAILGUN_DOMAIN"]),
-        auth=("api", app.config["MAILGUN_API_KEY"]),
-        data={"from": admin,
-              "to": [admin],
-              "subject": "EPIDISCO: New workflow submitted!",
-              "text": message})
+        'https://api.mailgun.net/v3/{}/messages'.format(app.config['MAILGUN_DOMAIN']),
+        auth=('api', app.config['MAILGUN_API_KEY']),
+        data={'from': admin,
+              'to': [admin],
+              'subject': 'EPIDISCO: New workflow submitted!',
+              'text': message})
